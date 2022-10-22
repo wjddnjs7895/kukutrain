0d0790dd41036332e0e90415b1df25dcf6fd68b2
wjddnjs7895 <wjddnjs7895@naver.com>
2022-10-22 19:14:54 +0900

    Modify ios/android LocY

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,9 +4,11 @@ import BottomContainer from '../components/Container/BottomContainer';
 import MapContainer from '../components/Container/MapContainer';
 import { getHeightPixel, getPixelToNumber } from '../utils/responsive';
 import { HEIGHT } from '../utils/responsive';
+import { deviceModel } from '../utils';
 
 function MainPage() {
-  const [locY, setLocY] = useState(HEIGHT - getPixelToNumber(getHeightPixel(205)));
+  const OFFSET__LOCY = deviceModel() === 'android' ? 210 : deviceModel() === 'ios' ? 105 : 105;
+  const [locY, setLocY] = useState(HEIGHT - getPixelToNumber(getHeightPixel(OFFSET__LOCY)));
   return (
     <PageStyled>
       <MapContainer />
---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+export function deviceModel() {
+  let currentOS;
+  const mobile = /iphone|ipad|ipod|android/i.test(navigator.userAgent.toLowerCase());
+
+  if (mobile) {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (userAgent.search('android') > -1) {
+      currentOS = 'android';
+
+      const deviceAgent =
+        'Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; device Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Safari/533.1';
+      let androidversion = parseFloat(deviceAgent.match(/Android\s+([\d\.]+)/)[1]);
+      document.querySelector('body').getAttribute('data-device', androidversion);
+    } else if (userAgent.search('iphone') > -1 || userAgent.search('ipod') > -1 || userAgent.search('ipad') > -1) {
+      currentOS = 'ios';
+    } else {
+      currentOS = 'else';
+    }
+  } else {
+    currentOS = 'nomobile';
+  }
+
+  document.querySelector('body').classList.add(currentOS);
+}
+deviceModel();
